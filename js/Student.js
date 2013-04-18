ee1cd69f82af330bcd2515f959591e83db660c0e
Dag Einar Monsen <me@dag.im>
2013-04-19 00:50:40 +1000

    Changes to Student class.

--- js/Student.js
+++ js/Student.js
@@ -1,24 +1,39 @@
-define(['CSVHandler'], function(CSVHandler) {
+define(['underscore', 'CSVHandler'], function(_, CSVHandler) {
 
-	function Student() {
-		
+	function Student(params) {
+    _.extend(this, params);
 	}
 
-	Student.structure = {
-			'student': Number,
-			'undergrad': Boolean,
-			'female': Boolean,
-			'local': Boolean,
-			'gpa': Number,
-      'course1': Number,
-      'course2': Number,
-      'course3': Number,
-      'course4': Number
-	}
+  Student.collection = [];
 
-	Student.getAll = function(callback) {
-		return CSVHandler.loadParse(Student, 'data/student.csv', callback);
-	}
+  Student.structure = {
+    'student': Number,
+    'undergrad': Boolean,
+    'female': Boolean,
+    'local': Boolean,
+    'gpa': Number,
+    'course1': Number,
+    'course2': Number,
+    'course3': Number,
+    'course4': Number
+  };
+
+	Student.getAll = function getAll() {
+		return CSVHandler.loadParse(Student, Student.structure, 'data/student.csv');
+	};
+
+  /**
+   * binary: undergrad, female, local
+   * numeric: gpa
+   */
+	Student.prototype.getSimilarity = function getSimilarity() {
+
+		var properties = _.pluck([this], 'undergrad', 'female', 'local')
+
+		// Binary: undergrad, female, local
+		// Cosine value: gpa
+
+	};
 
 	return Student;
 });
